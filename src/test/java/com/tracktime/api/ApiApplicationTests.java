14695c9dd42c1e53a9d114739a6ccb4aa4130091
Oscar Mangan <oscarmangan.ire@gmail.com>
2026-06-02 21:46:46 -0400

    Initial commit, new Spring app for the TrackTime API

---
+++ src/test/java/com/tracktime/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracktime.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
